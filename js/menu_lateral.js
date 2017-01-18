29665aa1f60bba2ffa8348b6c86599c9753e3b79
Angel Daravan <daravanangel@gmail.com>
2017-01-18 23:04:33 +0100

    Proyecto con lorem ipsum

---
+++ js/menu_lateral.js
@@ -0,0 +1,7 @@
+function abrir_menu() {
+	document.getElementById("menu_izquierda").style.display = "block";
+}
+
+function cerrar_menu() {
+	document.getElementById("menu_izquierda").style.display = "none";
+}
